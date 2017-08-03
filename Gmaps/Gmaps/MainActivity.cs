78a5e0934c84f43d2e87e5ac480e73e4c5f83263
Key-F <kirill2010.97@mail.ru>
2017-08-03 12:41:09 +0300

    works on emu and device

---
+++ Gmaps/Gmaps/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+using System;
+using Android.Gms.Maps.Model;
+
+namespace Gmaps
+{
+    [Activity(Label = "Gmaps", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, IOnMapReadyCallback
+    {
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.SetPosition(new LatLng(55.03, 27));
+            markerOptions.SetTitle("epta");
+            googleMap.AddMarker(markerOptions);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+            MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+        }
+    }
+}
+
